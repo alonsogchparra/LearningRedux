8a727d452cd525816ba053e48ff1f254d2b413a3
aliens9889 <alienx9889@gmail.com>
2017-02-22 00:15:51 -0400

    feat(redux-example): Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
@@ -96,10 +97,57 @@ var removeMovie = (id) => {
     };
 };
 
+// Map reducer and action generators
+// ----------------------
+
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+    switch (action.type) {
+        case 'START_LOCATION_FETCH':
+            return {
+                isFetching: true,
+                url:undefined
+            };
+        case 'COMPLETE_LOCATION_FETCH':
+            return {
+                isFetching: false,
+                url: action.url
+            };
+        default:
+            return state;
+    }
+
+};
+
+var startLocationFetch = () => {
+    return {
+        type: 'START_LOCATION_FETCH'
+    }
+};
+
+var completeLocationFetch = (url) => {
+    return {
+        type: 'COMPLETE_LOCATION_FETCH',
+        url
+    };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+    axios.get('https://ipinfo.io').then(function (res) {
+        var loc = res.data.loc;
+        var baseUrl = 'http://maps.google.com?q='
+
+        store.dispatch(completeLocationFetch(baseUrl + location));
+    });
+};
+
+
 var reducer = redux.combineReducers({
     name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map: mapReducer
 });
 
 var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -109,15 +157,24 @@ var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && wi
 var unsubscribe = store.subscribe(() => {
     var state = store.getState();
 
-    console.log('Name is ', state.name);
-    document.getElementById('app').innerHTML = state.name;
+    // console.log('Name is ', state.name);
+    // document.getElementById('app').innerHTML = state.name;
 
     console.log('New State ', store.getState());
+
+    if(state.map.isFetching) {
+        document.getElementById('app').innerHTML = 'Loading...';
+    } else if (state.map.url) {
+        document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">View Your Location</a>'
+    }
+
 });
 // unsubscribe();
 
 var currentSate = store.getState();
 console.log('currentState', currentSate);
+
+fetchLocation();
 
 store.dispatch(changeName('Andrew'));
 store.dispatch(addHobby('Running'));
